0019c62f7f50a2bc3b62ef2af1b9e387c0670957
Caleb DeLaBruere <caleb.delabruere@inficon.com>
2022-12-07 12:45:38 -0500

    feat: repin and adjust timings for adafruit feather

--- include/RBConfiguration.h
+++ include/RBConfiguration.h
@@ -1,13 +1,13 @@
 #pragma once
 // timings to hold notes on and off (milliseconds)
-constexpr unsigned int KICK_HOLD_ON = 30;
+constexpr unsigned int KICK_HOLD_ON = 33;
 constexpr unsigned int KICK_HOLD_OFF = 50;
 
-constexpr unsigned int PADS_HOLD_ON = 10;
+constexpr unsigned int PADS_HOLD_ON = 13;
 constexpr unsigned int PADS_HOLD_OFF = 40;
 
-constexpr unsigned int CYM_HOLD_ON = 25;
-constexpr unsigned int CYM_HOLD_OFF = 40;
+constexpr unsigned int CYM_HOLD_ON = 33;
+constexpr unsigned int CYM_HOLD_OFF = 50;
 
 // midi channel to listen on
 constexpr unsigned int MIDI_CHANNEL = 10;
--- src/main.cpp
+++ src/main.cpp
@@ -34,14 +34,15 @@ struct output_conf_t
 };
 
 static constexpr output_conf_t outputs[NUM_OUT] = {
-    [OUT_KICK] = {.pin = 2, .timing = &kickTiming},
-    [OUT_PAD_RED] = {.pin = A0, .timing = &padTiming},
-    [OUT_PAD_YELLOW] = {.pin = A1, .timing = &padTiming},
-    [OUT_PAD_BLUE] = {.pin = A2, .timing = &padTiming},
-    [OUT_PAD_GREEN] = {.pin = A3, .timing = &padTiming},
-    [OUT_CYM_YELLOW] = {.pin = 3, .timing = &cymTiming},
-    [OUT_CYM_BLUE] = {.pin = 4, .timing = &cymTiming},
-    [OUT_CYM_GREEN] = {.pin = 5, .timing = &cymTiming},
+    [OUT_KICK] = {.pin = 3, .timing = &kickTiming},
+    [OUT_PAD_RED] = {.pin = 6, .timing = &padTiming},
+    [OUT_PAD_YELLOW] = {.pin = 9, .timing = &padTiming},
+    [OUT_PAD_BLUE] = {.pin = 10, .timing = &padTiming},
+    [OUT_PAD_GREEN] = {.pin = 2, .timing = &padTiming},
+    [OUT_CYM_YELLOW] = {.pin = 16, .timing = &cymTiming},
+    [OUT_CYM_BLUE] = {.pin = 17, .timing = &cymTiming},
+    [OUT_CYM_GREEN] = {.pin = 4, .timing = &cymTiming},
+
 };
 
 struct output_state_t
