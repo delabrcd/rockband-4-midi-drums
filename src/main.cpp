e3bb0a7365e8b46561936995b01a51855250257e
Caleb DeLaBruere <caleb.delabruere@inficon.com>
2022-12-09 10:39:30 -0500

    chore: add clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,7 @@
 #include "RBConfiguration.h"
 #pragma GCC diagnostic error "-Wextra"
 
-enum output_t
-{
+enum output_t {
     FIRST_OUT,
     OUT_KICK = FIRST_OUT,
     OUT_PAD_RED,
@@ -17,42 +16,38 @@ enum output_t
     NUM_OUT,
 };
 
-struct output_timing_t
-{
-    const unsigned long hold;    // useconds
-    const unsigned long holdOff; // useconds
+struct output_timing_t {
+    const unsigned long hold;     // useconds
+    const unsigned long holdOff;  // useconds
 };
 
 static constexpr output_timing_t kickTiming = {.hold = KICK_HOLD_ON, .holdOff = KICK_HOLD_OFF};
-static constexpr output_timing_t padTiming = {.hold = PADS_HOLD_ON, .holdOff = PADS_HOLD_OFF};
-static constexpr output_timing_t cymTiming = {.hold = CYM_HOLD_ON, .holdOff = CYM_HOLD_OFF};
+static constexpr output_timing_t padTiming  = {.hold = PADS_HOLD_ON, .holdOff = PADS_HOLD_OFF};
+static constexpr output_timing_t cymTiming  = {.hold = CYM_HOLD_ON, .holdOff = CYM_HOLD_OFF};
 
-struct output_conf_t
-{
-    const int pin;
+struct output_conf_t {
+    const int              pin;
     const output_timing_t *timing;
 };
 
 static constexpr output_conf_t outputs[NUM_OUT] = {
-    [OUT_KICK] = {.pin = 2, .timing = &kickTiming},
-    [OUT_PAD_RED] = {.pin = A0, .timing = &padTiming},
+    [OUT_KICK]       = {.pin = 2, .timing = &kickTiming},
+    [OUT_PAD_RED]    = {.pin = A0, .timing = &padTiming},
     [OUT_PAD_YELLOW] = {.pin = A1, .timing = &padTiming},
-    [OUT_PAD_BLUE] = {.pin = A2, .timing = &padTiming},
-    [OUT_PAD_GREEN] = {.pin = A3, .timing = &padTiming},
+    [OUT_PAD_BLUE]   = {.pin = A2, .timing = &padTiming},
+    [OUT_PAD_GREEN]  = {.pin = A3, .timing = &padTiming},
     [OUT_CYM_YELLOW] = {.pin = 3, .timing = &cymTiming},
-    [OUT_CYM_BLUE] = {.pin = 4, .timing = &cymTiming},
-    [OUT_CYM_GREEN] = {.pin = 5, .timing = &cymTiming},
+    [OUT_CYM_BLUE]   = {.pin = 4, .timing = &cymTiming},
+    [OUT_CYM_GREEN]  = {.pin = 5, .timing = &cymTiming},
 };
 
-struct output_state_t
-{
-    output_state_t()
-    {
+struct output_state_t {
+    output_state_t() {
         triggeredAt = 0;
-        triggered = false;
+        triggered   = false;
     }
     unsigned long triggeredAt;
-    bool triggered;
+    bool          triggered;
 };
 
 static output_state_t outputStates[NUM_OUT];
@@ -61,30 +56,27 @@ static output_state_t outputStates[NUM_OUT];
 // buffer of recent notes instead. that way we could turn flams and such into
 // R+Y hits and so on. maybe consider velocity, too. turning this into mostly
 // configuration data might be nice, also.
-static bool outputForNote(const uint8_t &note, output_t &out)
-{
-    switch (note)
-    {
+static bool outputForNote(const uint8_t &note, output_t &out) {
+    switch (note) {
 #define MIDI_MAP(midi_note, rb_out) \
     case midi_note:                 \
         out = rb_out;               \
         break;
 #include "MidiMapping.tbl"
 #undef MIDI_MAP
-    default:
-        return false;
+        default:
+            return false;
     }
     return true;
 }
 
-static void writeOutput(const output_t &out, const bool &state)
-{
+static void writeOutput(const output_t &out, const bool &state) {
     digitalWrite(outputs[out].pin, state);
     outputStates[out].triggered = state;
 }
 
-static void noteOn([[maybe_unused]] const uint8_t &channel, const uint8_t &note, const uint8_t &velocity)
-{
+static void noteOn([[maybe_unused]] const uint8_t &channel, const uint8_t &note,
+                   const uint8_t &velocity) {
     if (velocity <= SENSITIVITY)
         return;
     output_t out;
@@ -93,9 +85,8 @@ static void noteOn([[maybe_unused]] const uint8_t &channel, const uint8_t &note,
 
     auto io = &outputs[out];
     auto st = &outputStates[out];
-    auto t = millis();
-    if (t - st->triggeredAt > io->timing->hold + io->timing->holdOff)
-    {
+    auto t  = millis();
+    if (t - st->triggeredAt > io->timing->hold + io->timing->holdOff) {
         writeOutput(out, HIGH);
         st->triggeredAt = t;
     }
@@ -103,32 +94,28 @@ static void noteOn([[maybe_unused]] const uint8_t &channel, const uint8_t &note,
 
 MIDI_CREATE_DEFAULT_INSTANCE()
 
-void setup()
-{
-    for (int out = FIRST_OUT; out < NUM_OUT; out++)
-    {
+void setup() {
+    for (int out = FIRST_OUT; out < NUM_OUT; out++) {
         pinMode(outputs[out].pin, OUTPUT);
         writeOutput(static_cast<output_t>(out), LOW);
     }
     MIDI.begin(MIDI_CHANNEL);
 }
 
-void loop()
-{
-    while (MIDI.read())
-    {
+void loop() {
+    while (MIDI.read()) {
         // you can technically add a note ON handler in the midi library, but i like this better
         auto location_byte = MIDI.getType();
-        auto channel = MIDI.getChannel();
-        auto note = MIDI.getData1();
-        auto velocity = MIDI.getData2();
+        auto channel       = MIDI.getChannel();
+        auto note          = MIDI.getData1();
+        auto velocity      = MIDI.getData2();
         if (location_byte == midi::MidiType::NoteOn)
             noteOn(channel, note, velocity);
     }
     auto t = millis();
-    for (auto out = 0; out < NUM_OUT; out++)
-    {
-        if (outputStates[out].triggered && t - outputStates[out].triggeredAt > outputs[out].timing->hold)
+    for (auto out = 0; out < NUM_OUT; out++) {
+        if (outputStates[out].triggered &&
+            t - outputStates[out].triggeredAt > outputs[out].timing->hold)
             writeOutput((output_t)out, LOW);
     }
 }
